92c141c755070c83db3652d4dc47c2cbde82243d
Tyler Scott <tascott@ucdavis.edu>
2021-06-16 09:13:36 -0700

    try new dynamics. measures
    prep for PMRC

--- code/analysis/call_base_model.R
+++ code/analysis/call_base_model.R
@@ -1,22 +1,24 @@
 
 
 require(pbapply)
-source('code/model/emersonscott_model_3skill.R')
+source('code/model/emersonscott_model.R')
 require(parallel)
 require(doParallel)
 
 cores = floor(detectCores() / 1.2)
 cl = makeCluster(cores)
-reps = 1000
+reps = 1e3
 registerDoParallel(cl)
 
 clusterEvalQ(cl,expr =  source('code/model/emersonscott_model.R'))
 
 incentive.set = list(c(0.1,0.5),c(0.3,0.7),c(0.5,0.9),c(0.1,0.9))
 
-res = foreach(i = 1:reps) %dopar% {
+res = foreach(i = 1:reps) %do% {
   simulation.control = list(stakeholders = 50,regulators = 0, convenors = 0 ,
-                            incentive.set = sample(incentive.set,1)[[4]],
+                            incentive.range  = runif(n = 1,min = 0.1,max = 0.9),
+                           # skill.set = runif(1,0.1,0.9),
+                            #capacity.set = runif(1,0.1,0.9),
                             uncertainty = runif(1,min = 0.25,2.25), 
                             n_pieces = 1,
                             min.payout = 0,
@@ -28,9 +30,8 @@ res = foreach(i = 1:reps) %dopar% {
   tryCatch({EmersonScottModel(simulation.control = simulation.control)},error = function(e) NULL)
 }
 
-saveRDS(res,paste0('../bucket_mount/verde_scratch/test_baserun.',reps/1e3,'k.RDS'))
+saveRDS(res,paste0('scratch/test_baserun.',reps,'reps.RDS'))
 rm(res);gc()
 stopCluster(cl)
-
 
 
